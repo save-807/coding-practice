2ef9035e3045f7e11bac9d44d8feb04f4f3beb4a
ceceking <3413323231@qq.com>
2026-08-01 15:29:48 +0800

    P3196 代码思路理顺及优化补充

--- 洛谷/BFS、DFS、并查集、树/P3916 图的遍历.cpp
+++ 洛谷/BFS、DFS、并查集、树/P3916 图的遍历.cpp
@@ -1,27 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N=1e5+7;
-vector<int>vis(N),ans(N);
+vector<int>ans(N,-1);
 vector<vector<int>>g(N);
-void dfs(int x,int max_num){
-    if(vis[x])return ;
-    vis[x]=1;
-    max_num=max(max_num,x);
-    for(int i=0;i<g[x].size();i++){
-        dfs(g[x][i],g[x][i]);
-    }
-}
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int n,m;
     cin>>n>>m;
-    for(int i=0;i<N;i++)ans[i]=i;
     for(int i=0;i<m;i++){
         int x,y;
         cin>>x>>y;
-        g[x].push_back(y);
+        g[y].push_back(x);
     }
-    for(int i=1;i<=n;i++){
-        if(!vis[i])dfs(i,i);
+    stack<int>sk;
+    for(int i=n;i>0;i--){
+        if(ans[i]!=-1 )continue;
+        sk.emplace(i);
+        ans[i]=i;
+        while(!sk.empty()){
+            int p=sk.top();
+            sk.pop();
+            ans[p]=i;
+            for(auto node:g[p]){
+                if(ans[node]!=-1)continue;
+                sk.emplace(node);
+                ans[node]=i;
+            }
+        }
     }
     for(int i=1;i<=n;i++){
         if(i!=1)cout<<" ";
